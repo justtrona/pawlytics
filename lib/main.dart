5c87b9ea1a903d4430675771e938c804509f537f
rownshi <formycapstone@gmail.com>
2025-08-12 17:15:24 +0800

    connected to database in supabase_flutter package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:pawlytics/views/get_start/login_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://qohuvrnpnxzmmhcrtzjf.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvaHV2cm5wbnh6bW1oY3J0empmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MzMxOTgsImV4cCI6MjA2OTIwOTE5OH0.PFEyJxReGbp8sUV0Y8PViUkRvHVUREx8Hlid3D2k3aw',
+  );
   runApp(const MyApp());
 }
 
